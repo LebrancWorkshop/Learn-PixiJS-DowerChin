a65c67bfbaa46cc96e9b2fc97b959078bde7a935
lebrancconvas <lebrancconvas@gmail.com>
2023-09-09 18:31:16 +0700

    ✨ feat: update mouse event

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,31 @@ app.renderer.background.color = config.backgroundColor;
 
 // Init.
 
+// Player.
+const player = PIXI.Sprite.from('/assets/image/spritesheet/raider/Raider_1/Idle.png');
+player.anchor.set(0.5);
+player.x = 100;
+player.y = 100;
+app.stage.addChild(player);
+
+// Mouse Interactions.
+app.stage.interactive = true;
+app.stage.on('pointermove', movePlayer);
+
+function movePlayer(event: PIXI.FederatedMouseEvent) {
+  let position = event.data.global;
+  player.x = position.x;
+  player.y = position.y;
+  console.log(position);
+};
+
 
 // Game Loop.
 app.ticker.add((deltaTime: number) => loop(deltaTime));
 
 function loop(deltaTime: number) {
   // Update.
+  app.stage.addChild(player);
 
 
   // Render.

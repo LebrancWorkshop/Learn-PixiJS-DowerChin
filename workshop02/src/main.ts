9c51303094663093b1db4f0ee6586afffb3c6664
lebrancconvas <lebrancconvas@gmail.com>
2023-09-09 19:18:53 +0700

    ✨ feat: add keyboard event

--- workshop02/src/main.ts
+++ workshop02/src/main.ts
@@ -1,19 +1,42 @@
 import { PIXI, app } from './renderer';
 import { config } from './config';
+import { IKey } from './@types';
 
 document.body.appendChild(app.view);
 app.renderer.background.color = config.backgroundColor;
 
 // Setup.
+let keys: IKey = {};
+let speed = 10;
 
 // Init.
+const player = PIXI.Sprite.from('/assets/image/spritesheet/knight/png/Idle (1).png');
+player.anchor.set(0.5);
+player.x = app.view.width / 2;
+player.y = app.view.height / 2;
+app.stage.addChild(player);
+
+window.addEventListener('keydown', keydownHandler);
+window.addEventListener('keyup', keyupHandler);
+
+function keydownHandler(event: KeyboardEvent) {
+  keys[event.code] = true;
+};
+
+function keyupHandler(event: KeyboardEvent) {
+  keys[event.code] = false;
+};
 
 // Game Loop.
 app.ticker.add((deltaTime: number) => loop(deltaTime));
 
 function loop(deltaTime: number) {
   // Update.
-
+  if(keys['ArrowRight']) {
+    player.x += speed;
+  } else if(keys['ArrowLeft']) {
+    player.x -= speed;
+  }
 
   // Render.
 
